9c9e1edfad0da2afd1395c095472e1ea4aa09b7a
yjzhang3 <56132313+yjzhang3@users.noreply.github.com>
2020-06-04 18:49:25 -0400

    First draft of ABM
    ContinuousSpace visualization still not working. Also move method would be ideal if it's random walk

---
+++ server.py
@@ -0,0 +1,24 @@
+from model import PopulationModel
+from mesa.visualization.ModularVisualization import ModularServer
+from SimpleContinuousModule import SimpleCanvas
+import matplotlib.pyplot as plt
+
+model = PopulationModel(10, 6, 12,12)
+
+for i in range(5):
+    model.step()
+
+
+def person_draw(agent):
+    return {"Shape": "circle", "r": 2, "Filled": "true", "Color": "Red"}
+
+
+persons_canvas = SimpleCanvas(person_draw, 500, 500)
+model_params = {
+    "N": 5,
+    "r":6,
+    "width": 100,
+    "height": 100,
+}
+
+server = ModularServer(PopulationModel, [persons_canvas], "Person", model_params)
