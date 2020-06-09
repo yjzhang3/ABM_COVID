ebb71f9130a1f73d951b78e00d9f44dc0fa1b016
yjzhang3 <yjzhang3@bu.edu>
2020-06-09 19:13:49 -0400

    correct weights for random infection

--- model_SIR.py
+++ model_SIR.py
@@ -23,13 +23,13 @@ class PersonAgent(Agent):
 
         self.infect_prob = prob
         
-        if self.infected == 1:
-            self.model.num_infected += 1
-            self.model.num_uninfected =self.model.num_agents-self.model.num_infected
+        if self.infect_state == 1:
+            self.model.num_I += 1
+            self.model.num_S =self.model.num_agents-self.model.num_I
         """ calculate infected and uninfected in the whole system """
 
-        print('initialized as ' + 'id: '+ str(self.unique_id) + ' infected? '+ str(self.infected)+'\n')
-        print('total infected: ' + str(self.model.num_infected)+'\n')
+        print('initialized as ' + 'id: '+ str(self.unique_id) + ' infected? '+ str(self.infect_state)+'\n')
+        print('total infected: ' + str(self.model.num_I)+'\n')
 
     def set_infect_state(self,st):
         """ setter of individual agent, set to S I or R """
@@ -54,25 +54,26 @@ class PersonAgent(Agent):
         """ get all the possible neighbors within a certain radius """
 
         print('there are ' + str(len(all_neighbors)) + ' neighbors')
-        print('agent id ' + self.unique_id + 'has the following neighbors: ' + '\n')
+        print('agent id ' + str(self.unique_id) + ' has the following neighbors: ' + '\n')
         for neighbor in all_neighbors:
             print(str(neighbor.unique_id))
 
 
 
         """ infection with probability """
-        if all_neighbors != [] and if self.infect_state == 1: # if any other agent is in my infection radius and I'm infected
+        if all_neighbors != [] and self.infect_state == 1: # if any other agent is in my infection radius and I'm infected
             neighbor_infect_state = []
             for other_agent in all_neighbors: # record a list of what the infection state is of my neighbors
-                neighbor_infect_state.append(other_agent)
-            weights = [prob,1-prob] # probability of infecting others, not infecting others
-            new_neighbor_infect_state = choices(neighbor_infect_state,weights = weights, k = len(all_neighbors))
+                neighbor_infect_state.append(other_agent.infect_state)
+                print('neighbor infected?', other_agent.infect_state)
+            weights = [self.infect_prob,1-self.infect_prob] # probability of infecting others, not infecting others
+            new_neighbor_infect_state = choices([1,0],weights = weights, k = len(all_neighbors))
             """ generate a random list of infected/uninfected """
 
             for i, other_agent in enumerate(all_neighbors): # now update their infection state
                 other_agent.set_infect_state(new_neighbor_infect_state[i])
                 if new_neighbor_infect_state[i] == 1: 
-                    print('agent id ' + self.unique_id + 'infected this neighbor id' + other_agent.unique_id)
+                    print('agent id ' , self.unique_id, ' infected this neighbor id ', other_agent.unique_id)
 
 
     def move(self):
@@ -103,10 +104,11 @@ class PopulationModel(Model):
     num_R = 0
     num_agents = 0
     
-    def __init__(self,N,r,width,height):
+    def __init__(self,N,r,p,width,height):
         self.num_agents = N   
         self.num_S = N
-        # self.required_social_dist = r
+        self.required_social_dist = r
+        self.infect_overallprob = p
         
         """ scheduler controls the order at which agents are activated """
         """ RandomActivation activates all the agents once per step """
@@ -117,7 +119,7 @@ class PopulationModel(Model):
 
         # create agents 
         for i in range(self.num_agents):
-            a = PersonAgent(i,self.required_social_dist,self)
+            a = PersonAgent(i,self.required_social_dist,self.infect_overallprob, self)
             # i is the id number
             self.schedule.add(a)
             """ add an agent to the schedule using add method """
@@ -132,3 +134,4 @@ class PopulationModel(Model):
     def step(self):
         """ advance the model by one step """
         self.schedule.step()
+        
